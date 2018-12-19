67d72e7d7626d9397b38193738f79cf10264a9a8
rockfeng0 <lkf20031988@163.com>
2018-12-19 14:29:51 +0800

    完成项目管理的增删改

--- src/components/project/Project.vue
+++ src/components/project/Project.vue
@@ -2,6 +2,7 @@
 <template>
     <div>
         <div>
+          <el-button size="mini" @click="form_handler">添加项目</el-button>
           <el-table :data="tableData" style="width: 100%">
             <el-table-column type="index" :index="indexMethod" width="180"/>
             <el-table-column prop="name" label="项目名称" width="180"/>
@@ -11,35 +12,30 @@
             <el-table-column prop="u_time" label="更新时间"/>
             <el-table-column label="操作">
                 <template slot-scope="scope">
-                  <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+                  <el-button size="mini" @click="form_handler(scope.$index, scope.row)">编辑</el-button>
                   <el-button size="mini" type="danger" @click="delete_project(scope.$index, scope.row)">删除</el-button>
                 </template>
             </el-table-column>
           </el-table>
         </div>
 
-        <h1>新增待测试项目</h1>
-        <form action="" method="post" class="form" role="form">
-            <div>
-                <label>项目名称:</label>
-                <input id="name" name="name" type="text" v-model="project.name" placeholder="project under test">
-            </div>
-
-            <div>
-                <label>待测模块:</label>
-                <input id="module" name="module" type="text" v-model="project.module" placeholder="module under test">
-            </div>
-
-            <div>
-                <label for="comment">备注:</label>
-                <input id="comment" name="comment" type="text" v-model="project.comment" placeholder="It's a comment here">
-            </div>
-            <input @click="create_project" id="submit" name="submit" type="button" value="提交">
-            <input @click="get_projects" id="search" name="search" type="button" value="查询">
-            <input @click="delete_project" id="delete" name="search" type="button" value="删除">
-            <input @click="edit_project" id="edit" name="edit" type="button" value="编辑">
-        </form>
-
+        <el-dialog :title="dialog_config.title" :visible.sync="dialog_config.edit_dialog_form_visible">
+          <el-form :label-position="dialog_config.label_position" label-width="80px" :model="project">
+            <el-form-item label="项目名称">
+              <el-input v-model="project.name"/>
+            </el-form-item>
+            <el-form-item label="待测模块">
+              <el-input v-model="project.module"/>
+            </el-form-item>
+            <el-form-item label="备注">
+              <el-input v-model="project.comment"/>
+            </el-form-item>
+          </el-form>
+          <div slot="footer">
+            <el-button @click="dialog_config.edit_dialog_form_visible=false">取 消</el-button>
+            <el-button type="primary" @click="submit_form">确 定</el-button>
+          </div>
+        </el-dialog>
     </div>
 </template>
 
@@ -49,6 +45,11 @@
         data: function () {
             return {
                 proj_url: "http://127.0.0.1:5000/project/manager",
+                dialog_config: {
+                  title: "",
+                  label_position: "right",
+                  edit_dialog_form_visible: false,
+                },
                 project: {},
                 tableData: []
             }
@@ -57,18 +58,42 @@
             this.get_projects()
         },
         methods: {
+
             indexMethod(index) {
               return index + 1;
             },
-            create_project: function() {
-                this.$axios.post(this.proj_url, this.project)
-                .then(function (response) {
-                    console.log(response);
+
+            form_handler: function(index, row){
+                // console.log(index, row, JSON.parse(JSON.stringify(row)));
+                this.dialog_config.edit_dialog_form_visible = true;
+                if(typeof(row)=="undefined") {
+                  this.dialog_config.title = "添加项目"
+                  this.project = {};
+                } else {
+                  this.dialog_config.title = "编辑项目"
+                  this.project=JSON.parse(JSON.stringify(row));
+                }
+            },
+
+            submit_form: function() {
+                var _this = this
+                this.dialog_config.edit_dialog_form_visible=false;
+                if(typeof(this.project.id) == "undefined") {
+                  // create_project
+                  var resp =this.$axios.post(this.proj_url, this.project)
+                }else {
+                  // update_project
+                  var resp = this.$axios.put(this.proj_url + "?proj_id=" + this.project.id, this.project)
+                }
+                resp.then(function (response) {
+                  // console.log(response);
+                  _this.get_projects();
                 })
                 .catch(function (error) {
                     console.log(error);
                 });
             },
+
             delete_project: function(index, row) {
                 // console.log(index, row);
                 var _this = this
@@ -80,22 +105,25 @@
                     console.log(error);
                 });
             },
-            edit_project: function(index, row) {
-                // console.log(index, row);
-                this.$axios.put(this.proj_url + "?proj_id=10", this.project)
+
+            get_projects: function() {
+                var _this = this
+                this.$axios.get(this.proj_url)
                 .then(function (response) {
-                  console.log("update ok");
+                    // console.log(response.data);
+                    // console.log(response.status);
+                    // console.log(response.statusText);
+                    // console.log(response.headers);
+                    // console.log(response.config);
+                    _this.tableData=response.data.result
                 })
                 .catch(function (error) {
                     console.log(error);
                 });
             },
-            handleEdit(index, row) {
-              console.log(index, row);
-            },
-            get_projects: function() {
+            get_project: function() {
                 var _this = this
-                this.$axios.get(this.proj_url)
+                this.$axios.get(this.proj_url + "?proj_id=" + row.id)
                 .then(function (response) {
                     // console.log(response.data);
                     // console.log(response.status);
--- src/components/test.vue
+++ src/components/test.vue
@@ -1,34 +1,45 @@
-<template>
-  <div class="block">
-    <span class="demonstration">完整功能</span>
-    <el-pagination
-      @size-change="handleSizeChange"
-      @current-change="handleCurrentChange"
-      :current-page="currentPage4"
-      :page-sizes="[10, 20, 30, 50, 100]"
-      :page-size="10"
-      layout="total, sizes, prev, pager, next, jumper"
-      :total="400">
-    </el-pagination>
+
+<template lang="html">
+  <div class="">
+    <el-radio-group v-model="labelPosition" size="small">
+      <el-radio-button label="left">左对齐</el-radio-button>
+      <el-radio-button label="right">右对齐</el-radio-button>
+      <el-radio-button label="top">顶部对齐</el-radio-button>
+    </el-radio-group>
+    <div style="margin: 20px;"></div>
+    <el-form :label-position="labelPosition" label-width="80px" :model="formLabelAlign">
+      <el-form-item label="名称">
+        <el-input v-model="formLabelAlign.name"></el-input>
+      </el-form-item>
+      <el-form-item label="活动区域">
+        <el-input v-model="formLabelAlign.region"></el-input>
+      </el-form-item>
+      <el-form-item label="活动形式">
+        <el-input v-model="formLabelAlign.type"></el-input>
+      </el-form-item>
+    </el-form>
   </div>
+
 </template>
 
 <script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
+
+<script>
   export default {
-    methods: {
-      handleSizeChange(val) {
-        console.log(`每页 ${val} 条`);
-      },
-      handleCurrentChange(val) {
-        console.log(`当前页: ${val}`);
-      }
-    },
     data() {
       return {
-        currentPage1: 5,
-        currentPage2: 5,
-        currentPage3: 5,
-        currentPage4: 4
+        labelPosition: 'right',
+        formLabelAlign: {
+          name: '',
+          region: '',
+          type: ''
+        }
       };
     }
   }
