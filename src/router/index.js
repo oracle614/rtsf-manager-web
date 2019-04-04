f608390a3bd9d8c5b88af84746f1536967e14138
rockfeng0 <lkf20031988@163.com>
2019-04-04 16:02:32 +0800

    调整生产环境和测试环境的配置

--- src/router/index.js
+++ src/router/index.js
@@ -5,11 +5,22 @@ import Project from '@/components/project/Project.vue'
 import Login from '@/components/login/Login.vue'
 import Register from '@/components/login/Register.vue'
 import NotFound from '@/components/404.vue'
-import test from '@/components/test.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
 
 Vue.use(Router)
 
 const routes = [
+  {
+    path: '/',
+    component: index
+  },
+  {
+    path: '/index',
+    component: index,
+    children: [
+        { path: '/project', component: Project, name: '项目主页' }
+    ]
+  },
   {
       path: '/login',
       component: Login,
@@ -29,39 +40,35 @@ const routes = [
       hidden: true
   },
   {
-    path: '/index',
-    component: index,
-    children: [
-        { path: '/project', component: Project, name: '项目主页' }
-    ]
-  },
-  {
       path: '*',
       hidden: true,
       redirect: { path: '/404' }
   },
   {
-    path: '/test',
-    component: test
-  }
+    path: '/hello',
+    component: HelloWorld
+  },
 ]
 
 const router = new Router({
   routes: routes
 })
 
-router.beforeEach((to, from, next) => {
-  if (to.path == '/login') {
-    sessionStorage.removeItem('token');
-  }
-  let token = JSON.parse(sessionStorage.getItem('token'));
-  if (to.path == '/register') {
-    next()
-  } else if (!token && to.path != '/login') {
-    next({ path: '/login' })
-  } else {
-    next()
-  }
-})
+// production or development
+if(process.env.NODE_ENV == "production") {
+  router.beforeEach((to, from, next) => {
+    if (to.path == '/login') {
+      sessionStorage.removeItem('token');
+    }
+    let token = JSON.parse(sessionStorage.getItem('token'));
+    if (to.path == '/register') {
+      next()
+    } else if (!token && to.path != '/login') {
+      next({ path: '/login' })
+    } else {
+      next()
+    }
+  })
+}
 
 export default router
